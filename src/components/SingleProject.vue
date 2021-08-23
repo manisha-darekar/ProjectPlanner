17389b4dca6d0dd8da376f35bc12e9d2d4122275
manisha-darekar <darekar.monu@gmail.com>
2021-08-23 21:10:44 +0530

    Initial commit

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="project">
+    <div class="actions">
+        <h3 @click="show">{{project.title}}</h3>
+        <div class="icons">
+            
+            
+            <span class="material-icons-outlined">done</span>
+        </div>
+        
+    </div>
+    <div v-if="showdetails" class="details">
+        <p>{{project.details}}</p>
+    </div>
+
+</div>
+    
+</template>
+<script>
+export default {
+    props:['project'],
+    data()
+    {
+        return{
+            showdetails:false
+        }
+    },
+    methods:{
+        show()
+        {
+            this.showdetails=!this.showdetails;
+        }
+    }
+    
+}
+</script>
+<style>
+.project{
+    margin: 20px auto;
+    background: white;
+    padding: 10px 10px;
+    border-radius: 3px;
+    box-shadow: 2px 3px 2px rgba(0,0,0,0.5);
+    border-left: 4px solid rgb(233, 9, 173);
+}
+h3{
+    cursor: pointer;
+}
+</style>
+
+
